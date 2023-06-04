383bc06e0a00f250df29b5a55a4c31b268216508
abduls <abdulsamad3125@gmail.com>
2023-06-04 21:24:49 +0000

    super-over-league-react

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="super-over-league-bg-container">
+    <h1 className="super-over-league-heading">Super Over League</h1>
+    <div>
+      <img
+        className="team-image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="team-image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
